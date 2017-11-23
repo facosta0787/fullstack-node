c8496d8d72d6ce5fe17399adac972ace9dbdc5af
Felipe Acosta <facosta0787@gmail.com>
2017-11-22 23:34:22 -0500

    Add babel polyfill to fix problem with async/await

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,26 @@
 import chalk from 'chalk'
 import express from 'express'
+import asyncify from 'express-asyncify'
 import bodyParser from 'body-parser'
-
+import database from 'db'
 const PORT = process.env.PORT || 3000
-const app = express()
 
-app.use(bodyParser.urlencoded({ extended:false }))
-app.use(bodyParser.json())
+function main(){
+  const app = asyncify(express())
 
-app.listen(PORT, () => {
-  console.log(chalk.green(`Server running and listening on http://localhost:${PORT}`))
-})
+  app.use(bodyParser.urlencoded({ extended:false }))
+  app.use(bodyParser.json())
+
+
+  app.get('/', async (req, res ) => {
+    const { User } = await database({ reset:false })
+    const users = await User.findAll()
+    res.send(users)
+  })
+
+  app.listen(PORT, () => {
+    console.log(chalk.green(`Server running and listening on http://localhost:${PORT}`))
+  })
+}
+
+main()
