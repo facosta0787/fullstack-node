d50dc2d0b8fe6d9a78ffc96248a08a209198f38f
Felipe Acosta <facosta0787@gmail.com>
2017-11-23 00:36:52 -0500

    [add] Add module bcrypt for hashing passwords

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,19 +3,34 @@ import express from 'express'
 import asyncify from 'express-asyncify'
 import bodyParser from 'body-parser'
 import database from 'db'
+import bcrypt from 'bcrypt'
 const PORT = process.env.PORT || 3000
 
-function main(){
+async function main(){
   const app = asyncify(express())
-
   app.use(bodyParser.urlencoded({ extended:false }))
   app.use(bodyParser.json())
 
+  const { User } = await database({ reset:false })
 
   app.get('/', async (req, res ) => {
-    const { User } = await database({ reset:false })
     const users = await User.findAll()
     res.send(users)
+  })
+
+  app.get('/user/create', async (req ,res) =>{
+    const hash = await bcrypt.hash('porque',10)
+    const user = await User.create({
+      user: 'jescobar',
+      name: 'Jose Escobar',
+      password: hash
+    })
+    res.send(user)
+  })
+
+  app.get('/user/compare', async (req, res) =>{
+    const result = await bcrypt.compare('porque','$2a$10$nsMb/rNFCK6i.Z1kfycUiO3G9WP7TRu5jaYl2TaPMxO/BDd4nUhnq')
+    res.send({result})
   })
 
   app.listen(PORT, () => {
